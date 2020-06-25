c562aedaf56332b9588964bf74c59d740300c01a
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-25 17:34:13 -0400

    Returned 200 Code from Flight API

--- flights.py
+++ flights.py
@@ -8,77 +8,52 @@ import os
 import pyflightdata
 from datetime import datetime
 
-from pyflightdata import FlightData
 from dotenv import load_dotenv
 import requests
 
-api=FlightData()
-api.get_countries()
-print({countries})
+load_dotenv()
 
 
 
 
+#Info Inputs and Data Requests
+
+#TODO: Pull from user input Flight Number
+
+f_flight = "UA2402"
+
+FLIGHT_KEY = os.environ.get("PLANE_API_KEY")
+flight = "UA2402"
+
+request_url = f"http://api.aviationstack.com/v1/flights?access_key={FLIGHT_KEY}"
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Info Inputs and Data Requests
-
-
-api_key = os.environ.get("PLANE_API_KEY")
-
-request_url = f"http://api.aviationstack.com/v1/flights?access_key={api_key}"
-access_key = api_key
-
-requests.get(request_url)
 
 response = requests.get(request_url)
-
 api_response = json.loads(response.text) 
+print(response.status_code)
+
+
+
+#TODO: Provide Arrival Airport and Arrival Time
 
 
 
 
+#flight = ("DAL1429")
+
+#for flight in api_response['results']:
+    #if (flight['live']['is_ground'] is False):
+       # print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
+        #    flight['airline']['name'],
+         #   flight['flight']['iata'],
+          #  flight['departure']['airport'],
+           # flight['departure']['iata'],
+            #flight['arrival']['airport'],
+            #flight['arrival']['iata']))
 
 
-
-response = requests.get(request_url, api_key)
-
-
-api_response = api_result.json()
-
-api_result = requests.get('http://api.aviationstack.com/v1/flights?acess_key', api_key)
-
-
-
-flight = ("AF5393")
-
-for flight in api_response['results']:
-    if (flight['live']['is_ground'] is False):
-        print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
-            flight['airline']['name'],
-            flight['flight']['iata'],
-            flight['departure']['airport'],
-            flight['departure']['iata'],
-            flight['arrival']['airport'],
-            flight['arrival']['iata']))
-
-
-for flight in api_response['airline']:
-    print("The airline is": {airline}) 
-    flight['airline']['name']
+#for flight in api_response['airline']:
+    #print("The airline is": {airline}) 
+    #flight['airline']['name']
