14dc70df9493da832ae08b6f9dab0cec5e12f564
amandanic11 <65553398+amandanic11@users.noreply.github.com>
2020-06-24 21:06:22 -0400

    Added customizable parameters to URL

--- maps.py
+++ maps.py
@@ -2,15 +2,33 @@ import os
 import json
 import requests
 from dotenv import load_dotenv
+import datetime
+
+
 
 load_dotenv()
 
-MAPS_KEY = os.environ.get("GOOGLE_API")
+MAPS_KEY = os.environ.get("MAPQUEST_API")
 
-request_url = f"https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key={MAPS_KEY}"
+#TODO: Code from, to, and time parameters based on user inputs
+f_street = "2175 E 15 St"
+f_city = "Brooklyn" 
+f_state = "NY"
+f_zip = "11229"
+
+t_street = "LaGuardia Airport"
+t_city = "Queens" 
+t_state = "NY"
+t_zip = "11371"
+
+flight_arrival = "2020-06-30T08:00"
+
+request_url = f"http://www.mapquestapi.com/directions/v2/optimizedroute?key={MAPS_KEY}&from={f_street},+{f_city},+{f_state},+{f_zip}&to={t_street},+{t_city},+{t_state},+{t_zip}&timeType=3&isoLocal={flight_arrival}"
 
 response = requests.get(request_url)
 response_data = json.loads(response.text)
 print(response_data)
-# print(response.status_code)
-# print(response.text)
+print(response.status_code)
+#TODO: Pull time information and departure time from response data
+
+
