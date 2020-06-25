c38c7ffe49504906553b1b7cbb73ba6e0e94b094
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-25 18:38:35 -0400

    Prints API Info on Active Flight

--- flights.py
+++ flights.py
@@ -15,43 +15,33 @@ load_dotenv()
 
 
 
-
 #Info Inputs and Data Requests
 
 #TODO: Pull from user input Flight Number
 
-f_flight = "UA2402"
+#f_flight = "UA2402"
 
 FLIGHT_KEY = os.environ.get("PLANE_API_KEY")
-flight = "UA2402"
+flight = "2745"
+airline = "DL"
 
-request_url = f"http://api.aviationstack.com/v1/flights?access_key={FLIGHT_KEY}"
+#estimated_runwway = "estimated_runway"
 
-
+request_url = f"http://api.aviationstack.com/v1/flights?access_key={FLIGHT_KEY}&airline_iata={airline}&flight_number={flight}&flight_status=active"
 
 
 response = requests.get(request_url)
 api_response = json.loads(response.text) 
-print(response.status_code)
+print(api_response)
+
+
+
+
 
 
 
 #TODO: Provide Arrival Airport and Arrival Time
 
-
-
-
-#flight = ("DAL1429")
-
-#for flight in api_response['results']:
-    #if (flight['live']['is_ground'] is False):
-       # print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
-        #    flight['airline']['name'],
-         #   flight['flight']['iata'],
-          #  flight['departure']['airport'],
-           # flight['departure']['iata'],
-            #flight['arrival']['airport'],
-            #flight['arrival']['iata']))
 
 
 #for flight in api_response['airline']:
