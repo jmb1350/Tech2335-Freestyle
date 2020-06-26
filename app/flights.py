38c46c9fbd8174485c7947e51d4ebd791b4af578
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-26 15:14:02 -0400

    Prints Destination Airport + Estimated Landing time

--- app/flights.py
+++ app/flights.py
@@ -22,10 +22,9 @@ load_dotenv()
 #f_flight = "UA2402"
 
 FLIGHT_KEY = os.environ.get("PLANE_API_KEY")
-flight = "2745"
-airline = "DL"
+flight = "1587"
+airline = "AA"
 
-#estimated_runwway = "estimated_runway"
 
 request_url = f"http://api.aviationstack.com/v1/flights?access_key={FLIGHT_KEY}&airline_iata={airline}&flight_number={flight}&flight_status=active"
 
@@ -33,18 +32,13 @@ request_url = f"http://api.aviationstack.com/v1/flights?access_key={FLIGHT_KEY}&
 response = requests.get(request_url)
 api_response = json.loads(response.text) 
 
+print(api_response)
+
+#TODO: Provide Arrival Airport and Arrival Time
+
 #https://stackoverflow.com/questions/25855276/parsing-json-with-python-typeerror-list-indices-must-be-integers-not-str/25855320
 #this was helpful in getting the list to respond correctly.
 print(api_response['data'][0]['arrival']['airport'])
 
+print(api_response['data'][0]['arrival']['estimated_runway'])
 
-
-
-
-#TODO: Provide Arrival Airport and Arrival Time
-
-
-
-#for flight in api_response['airline']:
-    #print("The airline is": {airline}) 
-    #flight['airline']['name']
