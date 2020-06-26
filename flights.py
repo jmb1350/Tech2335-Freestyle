a76fd1f65b5213f114cd568bce25476041d56aac
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-26 14:55:06 -0400

    Flight data now searchable

--- flights.py
+++ flights.py
@@ -32,9 +32,10 @@ request_url = f"http://api.aviationstack.com/v1/flights?access_key={FLIGHT_KEY}&
 
 response = requests.get(request_url)
 api_response = json.loads(response.text) 
-print(api_response)
 
-
+#https://stackoverflow.com/questions/25855276/parsing-json-with-python-typeerror-list-indices-must-be-integers-not-str/25855320
+#this was helpful in getting the list to respond correctly.
+print(api_response['data'][0]['arrival']['airport'])
 
 
 
