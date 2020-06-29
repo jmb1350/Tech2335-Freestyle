4200738782ca03144610e917a6ca53f54e00c606
jmb1350 <65556581+jmb1350@users.noreply.github.com>
2020-06-28 22:04:03 -0400

    Swapped to Sendgrid API

--- app/texts.py
+++ app/texts.py
@@ -1,19 +1,74 @@
-#texts.py
-import json
-import requests
-import datetime
+#Send the User a Reminder Email
+
+
 import os
-from datetime import datetime
 from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+
+client = SendGridAPIClient(SENDGRID_API_KEY)
+print("CLIENT:", type(client))
+send_at=
+
+subject = "Your flight is arriving soon!"
+
+html_content = "Almost time to leave!"
+print("HTML:", html_content)
+
+message = Mail(from_email=MY_ADDRESS, to_emails=MY_ADDRESS, subject=subject, html_content=html_content)
+
+try:
+    response = client.send(message)
+
+    print("RESPONSE:", type(response)) 
+    print(response.status_code) 
+    print(response.body)
+    print(response.headers)
+
+except Exception as e:
+    print("OOPS", e.message)
 
 
 
-import nexmo
 
-client = nexmo.Client(key='6c2e943c', secret='WvVKYyYuA8bIW5qc')
 
-client.send_message({
-    'from': '13024861693',
-    'to': '15165101124',
-    'text': 'Time to Leave!',
-})
+
+
+
+
+
+#3import sendgrid
+import os
+
+#sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+#data = {
+#  "personalizations": [
+#    
+#    {
+#      "to": [
+#        {
+#          "email": "test@example.com"
+#        }
+#      ],
+#      "subject": "Sending with SendGrid is Fun"
+#    }
+#  ],
+#  "from": {
+#    "email": "test@example.com"
+#  },
+#  "content": [
+#    {
+#      "type": "text/plain",
+#      "value": "and easy to do anywhere, even with Python"
+#    }
+#  ]
+#}
+#response = sg.client.mail.send.post(request_body=data)
+#print(response.status_code)
+#print(response.body)
+#print(response.headers)
